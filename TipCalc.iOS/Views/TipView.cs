fb1c5947d407749f3be091eadea439b600af747e
Alexander Ignatov <yalishanda@abv.bg>
2019-08-15 14:59:50 +0300

    Create and implement iOS app

---
+++ TipCalc.iOS/Views/TipView.cs
@@ -0,0 +1,32 @@
+﻿using UIKit;
+using TipCalc.Core.ViewModels;
+using MvvmCross.Platforms.Ios.Views;
+using MvvmCross.Binding.BindingContext;
+
+namespace TipCalcTutorial.iOS.Views
+{
+    public partial class TipView : MvxViewController<TipViewModel>
+    {
+        public TipView() : base("TipView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<TipView, TipViewModel>();
+            set.Bind(TipLabel).To(vm => vm.Tip);
+            set.Bind(GenerositySlider).To(vm => vm.Generosity);
+            set.Bind(SubTotalTextField).To(vm => vm.SubTotal);
+            set.Apply();
+
+            // Close the keyboard on tapping outside of it
+            View.AddGestureRecognizer(new UITapGestureRecognizer(() =>
+            {
+                this.SubTotalTextField.ResignFirstResponder();
+            }));
+        }
+    }
+}
+
